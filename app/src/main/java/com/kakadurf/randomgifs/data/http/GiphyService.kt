0b668ecddc67c188c06f3622a53fcb4c99214271
GalievBulat <kakadurf@gmail.com>
2021-08-15 13:02:13 +0300

    functionality: Gif list with random elements, Saved gifs list, Updatable, recreatable, internet disconnection safe

--- app/src/main/java/com/kakadurf/randomgifs/data/http/GiphyService.kt
+++ app/src/main/java/com/kakadurf/randomgifs/data/http/GiphyService.kt
@@ -8,7 +8,6 @@ import retrofit2.http.Url
 interface GiphyService {
     @GET("/v1/gifs/random")
     suspend fun getRandomGif(): GiphyHttpResponse
-
     @GET
     suspend fun getMedia(@Url url: String?): ResponseBody
 }
